8e1e3cc6156225a7fcfe016b8ef8ae4beebc0fad
gsyl <gsylvestre@gmail.com>
2017-12-14 09:04:10 +0100

    demo session start

---
+++ wis-sessions-demo/a.php
@@ -0,0 +1,25 @@
+<?php 
+//les sessions : 
+//permettent de garder des données en mémoire d'une page à l'autre
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sessions</title>
+</head>
+<body>
+    <h1>A</h1>
+    <ul>
+        <li><a href="a.php">A</a></li>
+        <li><a href="b.php">B</a></li>
+        <li><a href="c.php">C : page sécurisée</a></li>
+        <li><a href="d.php">D : supprime les données en session</a></li>
+    </ul>
+</body>
+</html>
---
+++ wis-sessions-demo/d.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sessions</title>
+</head>
+<body>
+    <h1>D</h1>
+    <ul>
+        <li><a href="a.php">A</a></li>
+        <li><a href="b.php">B</a></li>
+        <li><a href="c.php">C : page sécurisée</a></li>
+        <li><a href="d.php">D : supprime les données en session</a></li>
+    </ul>
+</body>
+</html>
